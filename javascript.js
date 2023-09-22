583c4b7688d27d708fc8d703962f5978c7f3b9e7
Maram <37120344+MaramAlqadasi@users.noreply.github.com>
2023-09-22 22:04:43 +0000

    add functions to handle changes(mode, size, color)

--- javascript.js
+++ javascript.js
@@ -1,78 +1,119 @@
-let grideLines=0;
-let hoverMetod="onecolor";
-let defultColor="#d89274";
+const defultSize=16;
+const defultMode="oneColor";
+const defultColor="#d89274";
+let currentSize=defultSize;
+let currentMode=defultMode;
+let currentColor=defultColor;
 const color=document.querySelector(".color");
 const range=document.getElementById("range1");
 const board=document.querySelector(".board");
 const titleSize=document.querySelector("#titleSize");
 const eraser=document.querySelector(".erase");
 const reset=document.querySelector(".reset");
-const rainbow=document.querySelector("rainbow");
+const rainbow=document.querySelector(".rainbow");
 const gray=document.querySelector(".gray");
+const oneColorBtn=document.querySelector(".one_color")
 const gridLine=document.querySelector(".grid");
-const size1=document.querySelector("#size1");
-const buttons=document.querySelectorAll(".button");
+const rangeTitle=document.querySelector("#size1");
 
-function getColor(){
-    console.log(this);
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function setCurrentSize(newSize){
+    currentSize=newSize;
+   
 }
 
-function setGrides(){
-    const size=this.value||16;
-    color.addEventListener("onchange",getColor);
-    titleSize.textContent=`${size} X ${size}`;
-    size1.textContent= `${size} X ${size}`;
-    
+function setCurrentColor(newColor) {
+    currentColor = newColor
+  }
 
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+  }
+
+function reloadBoard(){
+    setGrids(currentSize);
+    clearBoard();
+}
+function clearBoard(){
+    board.innerHTML="";
+}
+function changeSize(newSize){
+    setCurrentSize(newSize);
+    updateSizeValue(newSize);
+    reloadBoard();
+
+}
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+        if (currentMode === 'Rainbow') {
+          const randomR = Math.floor(Math.random() * 256)
+          const randomG = Math.floor(Math.random() * 256)
+          const randomB = Math.floor(Math.random() * 256)
+          e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+        } else if (currentMode === "oneColor") {
+            console.log(currentMode);
+          e.target.style.backgroundColor = currentColor;
+        } else if (currentMode === 'eraser') {
+          e.target.style.backgroundColor = '#fefefe'
+    }
+}
+function updateSizeValue(newSize) {
+     
+    titleSize.textContent=`${newSize} X ${newSize}`;
+    rangeTitle.textContent= `${newSize} X ${newSize}`;
+    
+  }
+
+  function activateButton(newMode) {
+    if (currentMode === 'Rainbow') {
+      rainbow.classList.remove('activ')
+    } else if (currentMode === 'oneColor') {
+      oneColorBtn.classList.remove('activ')
+    } else if (currentMode === 'eraser') {
+      eraser.classList.remove('activ')
+    }
+  
+    if (newMode === 'Rainbow') {
+      rainbow.classList.add('activ')
+    } else if (newMode === 'oneColor') {
+      oneColorBtn.classList.add('activ')
+    } else if (newMode === 'eraser') {
+      eraser.classList.add('activ')
+    }
+  }
+  
+range.addEventListener("change",e =>changeSize(e.target.value));
+//range.addEventListener("mouseover",e =>changeSize(e.target.value));
+color.oninput = (e) => setCurrentColor(e.target.value)
+oneColorBtn.onclick = () => setCurrentMode("oneColor")
+rainbow.onclick = () => setCurrentMode('Rainbow')
+eraser.onclick = () => setCurrentMode('eraser')
+reset.onclick = () => reloadBoard()
+//rainbow.addEventListener("click",setCurrentMode("RainbowColor"));
+
+
+
+function setGrids(size){
+    
     board.style=`grid-template-columns: repeat(${size}, 1fr);grid-template-rows: repeat(${size}, 1fr)`;
     for(let i=1;i<=size;i++){
         for(let j=1;j<=size;j++){
            const square= document.createElement("div");
-           square.style=`hieght:auto;width:auto`;
+           square.addEventListener("mouseover",changeColor);
+           square.addEventListener('mousedown', changeColor)
            board.append(square); 
-           if(grideLines==1){
-            square.style=`border:1px solid gray`;
-            
-           }
-           else if(hoverMetod==="onecolor"){
-            square.addEventListener("mouseover",()=> {
-            square.style=`background-color:${defultColor};`;});
-        
-           }
         }
     }
-  
-
 }
-buttons.forEach(button => {
-    button.addEventListener("click",()=>{
-        button.classList.toggle("activ");
-        console.log(button);
-        if(button.value==="Rainbow"){
-            hoverMetod="RainbowColor";
-            console.log("Rainbow");
 
-        }
-        else if(button.value==="Gray"){
-            hoverMetod==="Gray";
-            console.log("Gray");
 
-        }
-        else if(button.value==="GridLines"){
-        
-            console.log("GridLine");
-
-        }
-        else if(button.value==="Erase"){
-            hoverMetod="White";
-            defultColor="white";
-            console.log("Erase");
-
-        }
-    })
-    
-});
-range.addEventListener("change",setGrides);
-//range.addEventListener("mouseover",setGrides);
-
-setGrides();
+window.onload = () => {
+    setGrids(defultSize);
+    activateButton(defultMode);
+  }
